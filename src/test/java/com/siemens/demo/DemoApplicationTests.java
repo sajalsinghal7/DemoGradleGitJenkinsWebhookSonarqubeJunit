f19b254b862aa51d52923cbd95930e8a1ef6ebd6
sajal singhal <sajalsinghal7@gmail.com>
2019-02-05 12:38:37 +0530

    initial commit

---
+++ src/test/java/com/siemens/demo/DemoApplicationTests.java
@@ -0,0 +1,26 @@
+package com.siemens.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads1() {
+	}
+	@Test
+	public void contextLoads2() {
+	}
+	@Test
+	public void contextLoads3() {
+	}
+	@Test
+	public void contextLoads4() {
+	}
+
+}
+
